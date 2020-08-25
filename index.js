1a49e79e993a485218a13cb2ea1f5a94820c55c1
CHANDAN KUMAR NICK <Chandannick@gmail.com>
2020-08-26 00:10:15 +0530

    Created Contact List Form

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const path = require("path");
 
 const port = 8000;
 
 const app = express();
 
+var contactList = [
+  {
+    name: "Nick",
+    phone: "8319469688",
+  },
+  {
+    name: "Chandan",
+    phone: "1234567890",
+  },
+  {
+    name: "Ashish",
+    phone: "123099929302",
+  },
+];
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
+
 app.get("/", function (req, res) {
-  res.send("<h1>Cool it's running</h1>");
+  res.render("home", {
+    TITLE: "I am Nick",
+    contact_list: contactList,
+  });
+});
+
+app.post("/create-contact", function (req, res) {
+  contactList.push(req.body);
+  return res.redirect("back");
 });
 
 app.listen(port, function (err) {
